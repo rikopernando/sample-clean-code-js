7e2aace58d67d7dd088604cce38441be019893a6
Riko <rikopernando96@gmail.com>
2020-03-30 20:05:02 +0700

    initial commit

---
+++ public/src/app.js
@@ -0,0 +1,22 @@
+loadEvents();
+// load every event in the page
+function loadEvents(){
+  document.querySelector('form').addEventListener('submit',submit);
+}
+
+// submit data function
+function submit(e){
+  e.preventDefault();
+  let input = document.querySelector('input');
+  if(input.value != '')
+    addTask(input.value);
+  input.value = '';
+}
+
+// add tasks
+function addTask(task){
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = task;
+  ul.appendChild(li);
+}
